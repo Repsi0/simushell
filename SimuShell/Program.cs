0aa28e024f241ec0b2d36fb48246c2a0da6fe8f4
Repsi0 <repeerc710@gmail.com>
2020-06-10 14:27:34 -0500

    Cleaned up some more code, automatic/forced man pages, nicer ls and man output.

--- SimuShell/Program.cs
+++ SimuShell/Program.cs
@@ -12,15 +12,26 @@ namespace SimuShell
         public static string logPath = "~/.tmp/simushell-cmd.log";
 
         static List<ShellCommand> commands = new List<ShellCommand>();
+        public static string manPages = "\nSimuShell manual page:\n\n";
         public static string currentdir = "/";
+        public static string visibledir = currentdir;
 
         static string[] ConvertToCommand(string command) {
             return command.Split(' ');
         }
+        static string FixDirectory (string path) => (path.EndsWith('/')) ? path : path + '/';
+        static string ConvDirectory(string path) => (!path.StartsWith("/home/"        + System.Environment.UserName.ToLowerInvariant(),StringComparison.InvariantCulture) ? path
+                                                     : "~"+path.Remove(0, "/home/".Length + System.Environment.UserName.ToLowerInvariant().Length));
+        public static void RegisterCommand(string cmdName, Predicate<string[]> cmd, string manEntry) {
+            commands.Add(new ShellCommand(cmdName, cmd));
+            manPages += cmdName + " - " + manEntry + "\n";
+        }
+
         static void Interpret()
         {
             // Await next command
-            Console.Write(currentdir);
+            visibledir = ConvDirectory(currentdir);
+            Console.Write(visibledir);
             Console.Write(" - admin@SimuShell");
             Console.Write(">");
             string currentcommand = (Console.ReadLine());
@@ -36,12 +47,19 @@ namespace SimuShell
         public static void CommandExec(string[] command) {
             string cmd = command[0]; // Get main command from command/args array.
             // Find command with matching name
+            // Boolean so we don't have two commands with matching names, if so, warn the user/dev.
+            bool hasMatchedAlready = false;
+            ShellCommand sc_to_run = null;
             foreach(ShellCommand sc in commands) {
                 if (cmd == sc.cmdName) {
-                    sc.Execute(command.Skip(1).ToArray()); // Execute, passing only the arguments (not command).
-                    return;
+                    if (!hasMatchedAlready) {
+                        hasMatchedAlready = true;
+                        sc_to_run = sc;
+                    } else Console.WriteLine(cmd + " was found to be a duplicate command name!");
                 }
             }
+            if(!hasMatchedAlready) Console.WriteLine(cmd + ": command not found");
+            else sc_to_run.Execute(command.Skip(1).ToArray()); // Execute first command found, passing only the arguments (not command).
             Interpret();
         }
         static void Main() {
@@ -52,33 +70,39 @@ namespace SimuShell
             string if_START = Config.Get<string>("START-P");
             if (if_START == "on") { Console.WriteLine("Welcome to SimuShell. Type 'man' for manual."); }
             // Register commands
-            commands.Add(new ShellCommand("clear",    (Predicate<string[]>)delegate (string[] args) {
+            RegisterCommand("clear", (Predicate<string[]>)delegate (string[] args) {
                 Console.Clear();
                 return true;
-            }));
-            commands.Add(new ShellCommand("pwd",      (Predicate<string[]>)delegate (string[] args) {
+            }, "clear the terminal screen");
+            RegisterCommand("pwd", (Predicate<string[]>)delegate (string[] args) {
                 Console.WriteLine(currentdir);
                 return true;
-            }));
-            commands.Add(new ShellCommand("ls",       (Predicate<string[]>)delegate (string[] args) {
+            }, "print name of current/working directory");
+            RegisterCommand("ls", (Predicate<string[]>)delegate (string[] args) {
                 int amountoflisted = 0;
                 // Get all files + folders in directory
                 string[] filesindir = Directory.GetFiles(currentdir);
                 string[] foldsindir = Directory.GetDirectories(currentdir);
                 // Compile both into a single array
                 string[] complete = new string[filesindir.Length + foldsindir.Length];
-                complete.CopyTo(foldsindir, 0); complete.CopyTo(filesindir, foldsindir.Length);
+                foldsindir.CopyTo(complete, 0); filesindir.CopyTo(complete, foldsindir.Length);
                 bool isFile = false;
+                int numperline = 5;
+                int[] maxLength = new int[numperline];
+                for(int i = 0; i < complete.Length; i++) {
+                    int col = i%numperline;
+                    if (complete[i].Length > maxLength[col]) maxLength[col] = complete[i].Length;
+                }
                 for (int i = 0; i < complete.Length; i++)
                 {
                     // Appends '/', but only if it's a folder, and not a directory.
                     if (i >= foldsindir.Length && !isFile) isFile = true;
                     string newname = complete[i] + (isFile ? "" : "/");
                     // Add 3 spaces at the end of each name to make it look nice. (May change in future?)
-                    Console.Write(newname.PadRight(3 + newname.Length));
+                    Console.Write(newname.PadLeft(i%numperline==0?0:3).PadRight(3 + maxLength[i%numperline]));
                     // Insert a new line every 5 listed.
                     amountoflisted = amountoflisted + 1;
-                    if (amountoflisted == 5)
+                    if (amountoflisted == numperline)
                     {
                         // Only write another line if we're not on the last one (prevents from weird looking spacing at the end)
                         if (i != complete.Length - 1) Console.WriteLine("");
@@ -88,19 +112,25 @@ namespace SimuShell
                 // Write an additional line to return to normal shell appearance.
                 Console.WriteLine("");
                 return true;
-            }));
-            commands.Add(new ShellCommand("cd",       (Predicate<string[]>)delegate (string[] args) {
+            }, "list directory contents");
+            RegisterCommand("cd", (Predicate<string[]>)delegate (string[] args) {
                 string[] path = args[0].Split('/'); // Should be first argument; split into each directory
-                string newPath = currentdir;
+                // If path starts with /, start searching from /, if ~, start searching from user dir, else, just use current directory.
+                string newPath = args[0].StartsWith('/') ? "/" : 
+                                 (args[0].StartsWith('~') ? "/home/" + System.Environment.UserName : currentdir);
+                if (args[0].StartsWith('~')) path = args[0].Remove(0, 1).Split('/'); // Remove ~ if in user's directory.
                 foreach (string dir in path) {
                     if (dir == "..") {
-                        //Go back one -- if we're not at the beginning already
-                        newPath = newPath.LastIndexOf("/") == 0 ? newPath : newPath.Remove(newPath.LastIndexOf('/'));
+                        //Go back one -- double remove since there is an extra / at the end, but not if we're already at /
+                        if (newPath != "/") {
+                            newPath = newPath.Remove(newPath.LastIndexOf('/'));
+                            newPath = newPath.Remove(newPath.LastIndexOf('/'));
+                        }
                     }
                     else if (dir == ".") { } // Same directory; no change
                     else {
                         string[] dirs = Directory.GetDirectories(newPath);
-                        string pathPlusDir = newPath == "/" ? newPath + dir : newPath + "/" + dir; // Appends dir to path if path is /, otherwise adds another / before adding dir
+                        string pathPlusDir = newPath + dir;
                         // Check that directory exists
                         if (Array.Exists(dirs, directory => directory.Contains(pathPlusDir))) {
                             newPath = pathPlusDir;
@@ -109,37 +139,31 @@ namespace SimuShell
                             return false;
                         }
                     }
+                    newPath = FixDirectory(newPath);
                 }
                 currentdir = newPath;
                 return true;
-            }));
-            commands.Add(new ShellCommand("man",      (Predicate<string[]>)delegate (string[] args) {
-                string[] man = new string[6]; // Size must be the amount of commands in the array/man page
-                man[0] = "cd; change directory ('..' to go back a directory)";
-                man[1] = "list (ls); list all files and folders in directory";
-                man[2] = "dir; list current directory";
-                man[3] = "man; lists available commands";
-                man[4] = "exit (quit); exits SimuShell";
-                man[5] = "echo; prints text after it";
-                PrintValues(man);
+            }, "change current/working directory");
+            RegisterCommand("man", (Predicate<string[]>)delegate (string[] args) {
+                Console.WriteLine(manPages);
                 return true;
-            }));
-            commands.Add(new ShellCommand("exit",     (Predicate<string[]>)delegate (string[] args) {
+            }, "show the manuals (this text)");
+            RegisterCommand("exit", (Predicate<string[]>)delegate (string[] args) {
                 System.Environment.Exit(0);
                 return true; // Never run; but needed
-            }));
-            commands.Add(new ShellCommand("echo",     (Predicate<string[]>)delegate (string[] args) {
+            }, "exit SimuShell");
+            RegisterCommand("echo", (Predicate<string[]>)delegate (string[] args) {
                 Console.WriteLine(string.Join("", args));
                 return true;
-            }));
-            commands.Add(new ShellCommand("settings", (Predicate<string[]>)delegate (string[] args) {
+            }, "display a line of text");
+            RegisterCommand("settings", (Predicate<string[]>)delegate (string[] args) {
                 SUCC_SET();
                 return true;
-            }));
-            commands.Add(new ShellCommand("help",     (Predicate<string[]>)delegate (string[] args) {
+            }, "open SUCC config settings");
+            RegisterCommand("help", (Predicate<string[]>)delegate (string[] args) {
                 Console.WriteLine("Welcome to the SimuShell help prompt! Go away, this is under construction. GET OUT!");
                 return true;
-            }));
+            }, "show help text");
             // Enter the interpret loop
             Interpret();
         }
